e2f74b3c615ebc53b2dbbd8b078b810eeec191d7
no <no.wuttichai@gmail.com>
2018-09-05 18:12:33 +0700

    createUI
    Notification
    Exchange

--- Credit/ExchangeViewController.swift
+++ Credit/ExchangeViewController.swift
@@ -8,12 +8,68 @@
 
 import UIKit
 
-class ExchangeViewController: UIViewController {
+class ExchangeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    
+    @IBOutlet weak var collectionviewExchange: UICollectionView!
+    
+    let titleNameExchange = ["ขายสิทธิ อาบน้ำ 1 แถมหนึ่ง สถานที่ xxxxxx","ส่วนลดเที่ยวคาราโอเกะ 40 %","xxxxxxxxx","xxxxxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxxx"]
+    
+    let imagesModelExchange: [UIImage] = [
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionviewExchange.dataSource = self
+        collectionviewExchange.delegate = self
+        
+        collectionviewExchange!.contentInset = UIEdgeInsets(top: 10, left: 25, bottom: 30, right: 25) // Padding of collectviewHome
+        
+        let itemSize = UIScreen.main.bounds.width-35;
+        let itemHeight = itemSize*69/100
+        
+        let layout = UICollectionViewFlowLayout();
+        layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemHeight)
+        
+        layout.minimumLineSpacing = 12
+        layout.minimumInteritemSpacing = 3
+        
+        collectionviewExchange.collectionViewLayout = layout
+        
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return titleNameExchange.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionviewExchange.dequeueReusableCell(withReuseIdentifier: "cellExchange", for: indexPath) as! ExchangeCollectionViewCell
+        
+        cell.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowRadius = 3
+        cell.layer.shadowOpacity = 0.3
+        cell.layer.shadowOffset = CGSize(width: 1, height: 3)
+        
+        cell.titleListExchang.text = titleNameExchange[indexPath.item]
+        cell.imageListExchang.image = imagesModelExchange[indexPath.item]
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
--- Credit/NotificationViewController.swift
+++ Credit/NotificationViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionviewNoti: UICollectionView!
+    
+    let titleName = ["บริการนวดฟรี 1 คืน พร้อมเลขาส่วนตัว","ส่วนลด Night Club 30%","xxxxxxxxx","xxxxxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxxx","xxxxxxxxxxxxxx"]
+    
+    let imagesModel: [UIImage] = [
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        UIImage(named: "bedroom")!,
+        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionviewNoti.dataSource = self
+        collectionviewNoti.delegate = self
+        
+        collectionviewNoti!.contentInset = UIEdgeInsets(top: 10, left: 25, bottom: 30, right: 25) // Padding of collectviewHome
+        
+        let itemSize = UIScreen.main.bounds.width-35;
+        let itemHeight = itemSize*69/100
+        
+        let layout = UICollectionViewFlowLayout();
+        layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        layout.itemSize = CGSize(width: itemSize, height: itemHeight)
+
+        layout.minimumLineSpacing = 12
+        layout.minimumInteritemSpacing = 3
+        
+        collectionviewNoti.collectionViewLayout = layout
+        
+//        setCustomBackImage()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return titleName.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionviewNoti.dequeueReusableCell(withReuseIdentifier: "cellNoti", for: indexPath) as! NotificationCollectionViewCell
+        
+        cell.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowRadius = 3
+        cell.layer.shadowOpacity = 0.3
+        cell.layer.shadowOffset = CGSize(width: 1, height: 3)
+        
+        cell.titleNameNoti.text = titleName[indexPath.item]
+        cell.imageListNoti.image = imagesModel[indexPath.item]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
